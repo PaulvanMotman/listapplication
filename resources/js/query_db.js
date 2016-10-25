0a5300c25e5182dbb7120db8fb99df4260e582eb
Paul van Motman <pcvanmotman@gmail.com>
2016-10-25 21:31:04 +0200

    added comments and cleaned code up a bit

--- resources/js/query_db.js
+++ resources/js/query_db.js
@@ -1,16 +1,22 @@
+// Requiring postgres
 var pg = require('pg');
+// Store the connectionstring in a simple variable
 var connectionString = "postgres://Paul:postgres@localhost/bulletinboard";
 
+// export function to communicate with postgres
 module.exports = function (queryString, queryParameters, onComplete) {
-	if (typeof queryParameters == 'function') { // normalize parameters
+	// normalize parameters: if queryParameters doesn;t get used, the function only takes in two parameters, the 
+	// second being the callback
+	if (typeof queryParameters == 'function') { 
 		onComplete = queryParameters;
 		queryParameters = [];
 	}
-
+	// connect with the database bulletinboard
 	pg.connect(connectionString, function (err, client, done) {
+		// check for errors
 		if (err) {
 			console.log('error: connection to database failed. connection string: \"' + connectionString + '\" ' + err);
-
+			// disconnect the client
 			if (client) {
 				done(client);
 			}
@@ -21,15 +27,16 @@ module.exports = function (queryString, queryParameters, onComplete) {
 
 			return;
 		}
-
+		// fill out a query to postgres, inserting whatever queryString and queryParameters you've put in
 		client.query(queryString, queryParameters, function (err, result) {
+				// check for errors
 				if (err) {
 					done(client);
 					console.log('error: query failed: \"' + queryString + '\", \"' + queryParameters + "\" " + err);
 				} else {
 					done();
 				}
-
+				// call the callback
 				if (onComplete !== undefined) {
 					onComplete(err, result);
 				}
--- src/app.js
+++ src/app.js
@@ -1,26 +1,31 @@
+// Requring node modules
 const express = require('express');
 const bodyParser= require('body-parser')
 const app = express();
 
+// Require function exported in query_db.js
 var query = require('../resources/js/query_db');
 
+// Setting the view to jade files
 app.set('views', './src/vieuws');
 app.set('view engine', 'jade');
 
+// Use bodyparser to take input from request.body
 app.use(bodyParser.urlencoded({extended: true}))
+// Sending a folder of static files to the client side
 app.use(express.static('./resources/'));
 
 /// This part renders the form 
-
 app.get('/', (req, res) => {
   res.render("index")
  })
 
 /// This part renders the data from the database
-
 app.get('/view', (req, res) => {
+  // Select all rows from messages
   query('select * from messages', function (error, result) {
     console.log(result.rows);
+    // render the view with the data from the database
     res.render("view", {
       input: result.rows
     })
@@ -30,7 +35,9 @@ app.get('/view', (req, res) => {
 
 /// This part saves the data from the form into the database
 app.post('/quotes', (req, res) => {
+  // postgres query to store [req.body.name, req.body.quote], using ($1, $2), in the database
   query('insert into messages (title, body) values ($1, $2)', [req.body.name, req.body.quote], function (error, result) {
+    // check for errors
     if (error) {
       throw error
     }
